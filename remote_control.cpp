d9165a4e492ad0ec68855307e344171a005ae9b5
Kyle Grant <95764976+K-MkrOps@users.noreply.github.com>
2023-01-09 21:19:16 -0800

    Create remote_control.cpp

---
+++ remote_control.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+
+#include "brainwave_signal.h"
+#include "metasurface.h"
+
+// Maps brainwave attention intensity values to scattering patterns
+const std::unordered_map<int, ScatteringPattern> attentionIntensityMap = {
+    {1, ScatteringPattern::SPATIAL_HOLOGRAM},
+    {2, ScatteringPattern::BEAM_STEERING},
+    {3, ScatteringPattern::FOCUSED_BEAM},
+    {4, ScatteringPattern::WIDE_BEAM},
+};
+
+void handleRemoteControl(const BrainwaveSignal& signal, Metasurface& surface) {
+    int attentionIntensity = getAttentionIntensity(signal);
+    ScatteringPattern pattern = attentionIntensityMap.at(attentionIntensity);
+    surface.setScatteringPattern(pattern);
+}
